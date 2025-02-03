9c890d7ea8867d9da7888d3d67802bd214bd22f1
EmilyRochaTascaSenai <emily.rocha192@gmail.com>
2025-02-03 10:37:43 -0300

    colocando console.log

--- calculadora.js
+++ calculadora.js
@@ -1,13 +1,18 @@
 function calcular(){
     //obter os elementos a extrair o valor digitado(parseFloat)
     const precoEtanol = parseFloat(document.getElementById('preco-etanol').value);
+    console.log(precoEtanol)
     const precoGasolina= parseFloat(document.getElementById('preco-gasolina').value);
+    console.log(precoGasolina)
 
     //Sem parseFloat, pois nã será utilizado cálculo
     const combustivel=document.getElementById('combustivel').value;
+    console.log(combustivel)
 
     const gastoMedio=parseFloat(document.getElementById('gasto-medio').value);
+    console.log(gastoMedio)
     const distancia = parseFloat(document.getElementById('distancia').value);
+    console.log(distancia)
 }
 
 
