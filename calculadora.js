06cc7786a6b225adfe85dcb636306cce9a40311b
EmilyRochaTascaSenai <emily.rocha192@gmail.com>
2025-02-03 10:06:10 -0300

    começando js

--- calculadora.js
+++ calculadora.js
@@ -1,6 +1,8 @@
+function calcular(){
+    
+}
+
+
 function limpar(){
-    document.getElementById('resultado').style.display='none'
-    document.getElementById('valor01').value='';
-    document.getElementById('valor02') .value='';
 
 }
